7377b67151f14269c0b9c1e9df993e7252a1c96d
Maesro <maestrerodrigo@gmail.com>
2021-06-13 21:17:36 -0300

    Desafio 02 - Agregar navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { NavBarComponent } from "./components/NavBarComponent/NavBar";
+
+function App() {
+  return (
+    <div>
+
+        <NavBarComponent/>
+        <h2>Las ofertas de la semana</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBarComponent/NavBar.jsx
@@ -0,0 +1,17 @@
+import './style.css';
+
+export const NavBarComponent = () => {
+    return (
+            <div className="nav">
+                <nav>
+                    <h1>Esto es un Ecommerce</h1>
+                    <div className="secciones">
+                        <p>Zapatillas</p>
+                        <p>Indumentaria</p>
+                        <p>Accesorios</p>
+                    </div>
+                </nav>
+                
+            </div>
+    );
+}
