317c6ee951140f6cba187987cf328f669a9315ca
Maesro <maestrerodrigo@gmail.com>
2021-06-23 09:50:44 -0300

    Arreglo desafio4, se agrega onAdd

--- src/components/ItemCountComponent/ItemCount.jsx
+++ src/components/ItemCountComponent/ItemCount.jsx
@@ -5,16 +5,24 @@ import { useState } from 'react';
 
 export const ItemCountComponent = ({stock, initial, onAdd}) => {
     const[cant, setCant] = useState(initial)
-    console.log(cant)
+    
+    const quitarCantidad =()=>{
+        setCant(cant<=0?cant-0:cant-1)
+    } 
+    const agregarCantidad =()=>{
+        setCant(cant<stock?cant+1:cant+0)
+    } 
+ 
+
     return (            
         <div className="Item">
             <div className="contador">
-            <button onClick={()=>{setCant(cant<=0?cant-0:cant-1)}} className="contador__resta">-</button>
+            <button onClick={quitarCantidad} className="contador__resta">-</button>
             <p className="contador__resultado"> {cant}</p>
-            <button onClick={()=>{setCant(cant<stock?cant+1:cant+0)}} className="contador__suma">+</button>
+            <button onClick={agregarCantidad} className="contador__suma">+</button>
             </div>
             <div className="item_add">
-                <Button variant="outlined" color="primary">
+                <Button onClick={()=>onAdd(cant)} variant="outlined" color="primary">
                 Agregar al Carrito
                 </Button>
             </div>
